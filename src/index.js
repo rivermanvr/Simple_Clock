fce17440a673f42ae4672c9dc0200bf7d82a04ca
rivermanvr <vrgithub@riversconsulting.com>
2017-03-26 13:44:55 -0400

    turned clock into a class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-function tick() {
-  const element = (
-    <div className="container">
-      <h3>A simple Clock...</h3>
-      <h4>It is {new Date().toLocaleTimeString()}.</h4>
-    </div>
-  );
-  ReactDOM.render(element,document.getElementById('root'));
+import React, { Component } from 'react';
+import ReactDOM from 'reactDOM';
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <h3>A simple Clock...</h3>
+        <h4>It is {this.state.date.toLocaleTimeString()}.</h4>
+      </div>
+    );
+  }
 }
 
-setInterval(tick, 1000);
+ReactDOM.render(<Clock />,document.getElementById('root'));
